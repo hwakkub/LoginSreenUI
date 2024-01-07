fbb28bb388c4d361da0cbb1d315a0cccaa260c6c
hwakkub <152359933+hwakkub@users.noreply.github.com>
2024-01-08 00:36:09 +0700

    Initial Commit

---
+++ LoginSreenUI/LoginSreenUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginSreenUIApp.swift
+//  LoginSreenUI
+//
+//  Created by jirakit on 8/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct LoginSreenUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
